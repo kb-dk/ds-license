54c906009f50c0572decf12286297e188c3c1e9d
Thomas Egense <teg@kb.dk>
2022-04-29 13:50:07 +0200

    All JSP converted and compiles. css/js files also added.

--- src/main/java/dk/kb/license/webservice/ContextListener.java
+++ src/main/java/dk/kb/license/webservice/ContextListener.java
@@ -1,6 +1,10 @@
 package dk.kb.license.webservice;
 
 import java.io.IOException;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -8,6 +12,9 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import dk.kb.license.config.ServiceConfig;
+import dk.kb.license.storage.*;
+import dk.kb.license.util.H2DbUtil;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,18 +41,73 @@ public class ContextListener implements ServletContextListener {
             InitialContext ctx = new InitialContext();
             String configFile = (String) ctx.lookup("java:/comp/env/application-config");
             //TODO this should not refer to something in template. Should we perhaps use reflection here?
-            ServiceConfig.initialize(configFile);
-        } catch (NamingException e) {
+            ServiceConfig.initialize(configFile);                                                          
+            initialiseStorage();  
+        }
+        catch (NamingException e) {
             throw new RuntimeException("Failed to lookup settings", e);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load settings", e);        } 
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Failed to load settings", e);
+        } 
         log.info("Service initialized.");
+    }
+    
+    
+    /*
+     * Must be called after properties are initialized
+     */
+    public void initialiseStorage() {
+       log.info("Initializing storage");
+       
+       String driver = ServiceConfig.getDBDriver();
+       String url = ServiceConfig.getDBUrl();
+       String user = ServiceConfig.getDBUserName();
+       String password = ServiceConfig.getDBPassword();
+                     
+       //If running jetty for development
+       if ("org.h2.Driver".equals(driver)) { //Would be slightly better if we can detect it is jetty in local environment
+         createLocalH2ForJettyEnvironment(driver, url, user, password);
+       }
+       
+       LicenseModuleStorage.initialize(driver,url,user,password);                        
     }
 
 
+    private void createLocalH2ForJettyEnvironment(String driver, String url, String user, String password) {
+        try {
+         log.info("Setting up H2 database under jetty in development mode");          
+         H2DbUtil.createEmptyH2DBFromDDL(url, driver,  user, password);
+       }
+       catch(Exception e) {
+         log.error("Unable to create local h2 database for jetty environment",e);             
+       }
+    }
+    
+
+    // this is called by the web-container at shutdown. (defined in web.xml)
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        log.debug("Service destroyed");
+        try {
+         log.info("Shutdown service v{}", getClass().getPackage().getImplementationVersion());
+         LicenseModuleStorage.shutdown();
+            
+            Enumeration<Driver> drivers = DriverManager.getDrivers();
+            
+            while (drivers.hasMoreElements()) {
+                Driver driver = drivers.nextElement();
+                
+                try {
+                   log.debug("deregistering jdbc driver: {}", driver);
+                    DriverManager.deregisterDriver(driver);
+                } catch (SQLException e) {
+                    log.debug("Error deregistering driver {}", driver, e);
+                }
+            }            
+        } catch (Exception e) {
+            log.error("failed to shutdown Ds-Storage", e);
+        }
+        
     }
 
 }
---
+++ src/main/webapp/list_licenses.jsp
@@ -0,0 +1,36 @@
+<%@ page import="
+    java.util.*,
+     dk.kb.license.storage.*,
+     dk.kb.license.Util,
+     dk.kb.license.MonitorCache,
+     dk.kb.license.facade.LicenseModuleFacade"%>
+
+<%  
+   ArrayList<License> licenses = LicenseModuleFacade.getAllLicenseNames();
+%>
+  <table class="table table-condensed table-hover">
+   <thead>
+   <tr>
+    <th>ID</th>
+    <th>Name</th>
+    <th>Valid from</th>
+   <th>Valid to</th>
+   </tr>
+   </thead>
+   <tbody>
+<% for (int i = 0;i< licenses.size();i++ ){
+  License current = licenses.get(i);
+%>
+   <tr class="<%=Util.getStyle(i)%>" onclick="window.location = 'license.jsp?licenseId=<%=current.getId()%>';">
+      <td><%=current.getId()%></td>
+      <td><%=current.getLicenseName()%><br><%=current.getLicenseName_en()%></td>
+      <td><%=current.getValidFrom()%></td>
+      <td><%=current.getValidTo()%></td>
+  </tr>
+<%}%>
+   </tbody>
+</table>
+  
+ <a class="btn btn-primary" href="license.jsp?createNew=true">Opret ny licens</a>
+  
+  
