39ad254f8dc0da86035bf92b8066e8e33528f88f
Thomas Egense <teg@kb.dk>
2022-06-14 10:30:31 +0200

    Slowly converting and removing old DTO objects to open API

--- docs/ValidateAccessOutputDto.md
+++ docs/ValidateAccessOutputDto.md
@@ -6,7 +6,7 @@
 
 Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
-**accesss** | **Boolean** |  |  [optional]
+**access** | **Boolean** |  |  [optional]
 
 
 
--- src/test/java/dk/kb/license/integrationtest/LicenseModuleRestWSTester.java
+++ src/test/java/dk/kb/license/integrationtest/LicenseModuleRestWSTester.java
@@ -3,11 +3,22 @@ package dk.kb.license.integrationtest;
 import java.util.ArrayList;
 
 import javax.ws.rs.core.MediaType;
+
 import org.apache.cxf.jaxrs.client.WebClient;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 
-import dk.kb.license.webservice.dto.*;
+import dk.kb.license.model.v1.CheckAccessForIdsInputDto;
+import dk.kb.license.model.v1.CheckAccessForIdsOutputDto;
+import dk.kb.license.model.v1.GetUserGroupsInputDto;
+import dk.kb.license.model.v1.GetUserGroupsOutputDto;
+import dk.kb.license.model.v1.GetUserQueryInputDto;
+import dk.kb.license.model.v1.GetUsersLicensesInputDto;
+import dk.kb.license.model.v1.GetUsersLicensesOutputDto;
+import dk.kb.license.model.v1.UserObjAttributeDto;
+import dk.kb.license.model.v1.ValidateAccessInputDto;
+import dk.kb.license.model.v1.ValidateAccessOutputDto;
+
 
 
 
@@ -40,9 +51,9 @@ public class LicenseModuleRestWSTester {
 	@SuppressWarnings("all")
 	private static void testValidateAccess() throws Exception {
 		// Test Validate Access
-		ValidateAccessInputDTO input = new ValidateAccessInputDTO();
+		ValidateAccessInputDto input = new ValidateAccessInputDto();
 
-		ArrayList<UserObjAttributeDTO> userObjAttributes = createTestUserObjAttributeDTO();
+		ArrayList<UserObjAttributeDto> userObjAttributes = createTestUserObjAttributeDto();
 		input.setAttributes(userObjAttributes);
 
 		ArrayList<String> groups = new ArrayList<String>();
@@ -52,17 +63,17 @@ public class LicenseModuleRestWSTester {
 				
 	    WebClient client = getWebClient();
 			    	   
-	    ValidateAccessOutputDTO post = client.path("validateAccess").type(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).post(input,ValidateAccessOutputDTO.class);	    
-	    System.out.println(post.isAccess()); //true of false
+	    ValidateAccessOutputDto post = client.path("validateAccess").type(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).post(input,ValidateAccessOutputDto.class);	    
+	    System.out.println(post.getAccess()); //true of false
 	}
 
 	
 	
 	@SuppressWarnings("all")
 	private static void testGetUserLicenseQuery() throws Exception {
-		GetUserQueryInputDTO input = new GetUserQueryInputDTO();
+		GetUserQueryInputDto input = new GetUserQueryInputDto();
 		input.setPresentationType("Search");
-		input.setAttributes(createTestUserObjAttributeDTO());				
+		input.setAttributes(createTestUserObjAttributeDto());				
 		 WebClient client = getWebClient();
 		String output = client.path("getUserLicenseQuery").type(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).post(input,String.class);
 
@@ -73,19 +84,19 @@ public class LicenseModuleRestWSTester {
 	
 	@SuppressWarnings("all")
 	private static void testCheckAccessForIds() throws Exception {
-		CheckAccessForIdsInputDTO input = new CheckAccessForIdsInputDTO();
+		CheckAccessForIdsInputDto input = new CheckAccessForIdsInputDto();
 		input.setPresentationType("Search");
-		input.setAttributes(createTestUserObjAttributeDTO());
+		input.setAttributes(createTestUserObjAttributeDto());
         ArrayList<String> ids = new ArrayList<String>();        
         ids.add("doms_radioTVCollection:uuid:371157ee-b120-4504-bfaf-364c15a4137c");//radio TV        
         ids.add("doms_radioTVCollection:uuid:c3386ed5-9b79-47a2-a648-8de53569e630");//radio TV
 		ids.add("doms_reklamefilm:uuid:35a1aa76-97a1-4f1b-b5aa-ad2a246eeeec"); //reklame
 		ids.add("doms_newspaperCollection:uuid:18709dea-802c-4bd7-98e6-32ca3b285774-segment_6"); //aviser		
-		input.setIds(ids);
+		input.setAccessIds(ids);
 				
 	  WebClient client = getWebClient();
 			
-		CheckAccessForIdsOutputDTO output = client.path("checkAccessForIds").type(MediaType.TEXT_XML).accept(MediaType.TEXT_XML).post(input,CheckAccessForIdsOutputDTO.class);
+		CheckAccessForIdsOutputDto output = client.path("checkAccessForIds").type(MediaType.TEXT_XML).accept(MediaType.TEXT_XML).post(input,CheckAccessForIdsOutputDto.class);
 		System.out.println("query:" + output.getQuery());
 		System.out.println("presentationtype:" + output.getPresentationType());
 		System.out.println("number of IDs:" + output.getAccessIds().size());	
@@ -96,12 +107,12 @@ public class LicenseModuleRestWSTester {
 	private static void testGetUsersLicenses() throws Exception {
 		// GetUserLicensesOutputDTO getUserLicenses
 
-		GetUsersLicensesInputDTO input = new GetUsersLicensesInputDTO();
-		input.setAttributes(createTestUserObjAttributeDTO());
+		GetUsersLicensesInputDto input = new GetUsersLicensesInputDto();
+		input.setAttributes(createTestUserObjAttributeDto());
         input.setLocale("da");
 		        
         WebClient client = getWebClient();
-		GetUsersLicensesOutputDTO output = client.path("getUserLicenses").type(MediaType.TEXT_XML).accept(MediaType.TEXT_XML).post(input,GetUsersLicensesOutputDTO.class);
+		GetUsersLicensesOutputDto output = client.path("getUserLicenses").type(MediaType.TEXT_XML).accept(MediaType.TEXT_XML).post(input,GetUsersLicensesOutputDto.class);
 					
 		System.out.println("output, licensenames:" + output.getLicenses());
 	}
@@ -112,21 +123,21 @@ public class LicenseModuleRestWSTester {
 	
 	@SuppressWarnings("all")
 	private static void testGetUsersGroups() throws Exception {
-		GetUserGroupsInputDTO input = new GetUserGroupsInputDTO();
-		input.setAttributes(createTestUserObjAttributeDTO());
+		GetUserGroupsInputDto input = new GetUserGroupsInputDto();
+		input.setAttributes(createTestUserObjAttributeDto());
         input.setLocale("da");        
         WebClient client = getWebClient();
-		GetUserGroupsOutputDTO output = client.path("getUserGroups").type(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).post(input,GetUserGroupsOutputDTO.class);	
+		GetUserGroupsOutputDto output = client.path("getUserGroups").type(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON).post(input,GetUserGroupsOutputDto.class);	
 		System.out.println("output, groups:" + output.getGroups());
 	}
 
 	
 
 	
-	private static ArrayList<UserObjAttributeDTO> createTestUserObjAttributeDTO() {
-		ArrayList<UserObjAttributeDTO> userObjAttributes = new ArrayList<UserObjAttributeDTO>();
+	private static ArrayList<UserObjAttributeDto> createTestUserObjAttributeDto() {
+		ArrayList<UserObjAttributeDto> userObjAttributes = new ArrayList<UserObjAttributeDto>();
 	
-		UserObjAttributeDTO newUserObjAtt = new UserObjAttributeDTO();
+		UserObjAttributeDto newUserObjAtt = new UserObjAttributeDto();
 		userObjAttributes.add(newUserObjAtt);
 		newUserObjAtt.setAttribute("attribut_store.MediestreamFullAccess");
 		ArrayList<String> values = new ArrayList<String>();
@@ -135,14 +146,14 @@ public class LicenseModuleRestWSTester {
 		newUserObjAtt.setValues(values);
 		
 		
-		UserObjAttributeDTO newUserObjAtt1 = new UserObjAttributeDTO();
+		UserObjAttributeDto newUserObjAtt1 = new UserObjAttributeDto();
         userObjAttributes.add(newUserObjAtt1);
         newUserObjAtt1.setAttribute("mail");
         ArrayList<String> values1 = new ArrayList<String>();
         values1.add("mvk@statsbiblioteket.dk");
         newUserObjAtt1.setValues(values1);
 		        
-        UserObjAttributeDTO newUserObjAtt2 = new UserObjAttributeDTO();
+        UserObjAttributeDto newUserObjAtt2 = new UserObjAttributeDto();
         userObjAttributes.add(newUserObjAtt2);
         newUserObjAtt2.setAttribute("SBIPRoleMapper");
         ArrayList<String> values2 = new ArrayList<String>();
