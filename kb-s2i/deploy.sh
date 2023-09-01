cd216a47b7ac5447e673cde878d7ee3bb017f55e
Toke Eskildsen <te@ekot.dk>
2023-09-01 13:38:37 +0200

    Adjust deployment descriptor to use less generic logback config filename

--- kb-s2i/deploy.sh
+++ kb-s2i/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cp -- /tmp/src/conf/ocp/logback.xml "$CONF_DIR/logback.xml"
+cp -- /tmp/src/conf/ocp/ds-license.ds-license.logback.xml "$CONF_DIR/logback.xml"
 # There are normally two configurations: core and environment
 cp -- /tmp/src/conf/ds-license-*.yaml "$CONF_DIR/"
  
