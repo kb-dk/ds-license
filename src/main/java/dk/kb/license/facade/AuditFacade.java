acdf5944b34f8e29cd2d08e8872de68705b3b03c
Jonathan Fredgaard <jfre@kb.dk>
2025-07-01 10:39:31 +0200

    DRA-2028-AUDIT - added java docs

--- src/main/java/dk/kb/license/facade/AuditFacade.java
+++ src/main/java/dk/kb/license/facade/AuditFacade.java
@@ -3,18 +3,21 @@ package dk.kb.license.facade;
 import java.util.ArrayList;
 
 import dk.kb.license.model.v1.AuditEntryOutputDto;
+import dk.kb.license.storage.AuditLogEntry;
 import dk.kb.license.storage.BaseModuleStorage;
 import dk.kb.license.storage.LicenseModuleStorage;
+import dk.kb.license.storage.PresentationType;
 
 
 public class AuditFacade {
 
     
     /**
-     * Retrieve a specific audit log by id.
-     * <p>
+     * Retrieves a {@link AuditLogEntry} by id
+     * This method does not have an immediate usecase.
+     *
      * @param auditLogId The ID of the log.
-     * @return AuditLog The auditlog with this id
+     * @return the AuditLogEntry with the given id
      */
     public static AuditEntryOutputDto getAuditEntryById(Long auditLogId) {
         return BaseModuleStorage.performStorageAction("getAuditEntries()", LicenseModuleStorage.class, storage -> {
@@ -25,10 +28,10 @@ public class AuditFacade {
 
     
     /**
-     * A list of audit entries for a given object id
-     * <p>
-     * @param objectId The ID of the object to retrieve audit entries for. 
-     * @return AuditLog The auditlog with this id
+     * Retrieves a list of all {@link AuditLogEntry} related to a given object.
+     *
+     * @param objectId The ID of the object for which to retrieve audit entries.
+     * @return A list of AuditLogEntry related to
      */
     public static ArrayList<AuditEntryOutputDto> getAuditEntriesByObjectId(Long objectId) {
         return BaseModuleStorage.performStorageAction("getAuditLogByObjectId()", LicenseModuleStorage.class, storage -> {
